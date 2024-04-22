8e10b300333ce739ff04cdde627cc44fb7573b37
yuna <yuna2101@naver.com>
2024-04-22 23:55:05 +0900

    프로그래머스 Lv.0 점의 위치 구하기

--- Day_0422/src/programers/Day0422_programers2.java
+++ Day_0422/src/programers/Day0422_programers2.java
@@ -21,15 +21,6 @@ public class Day0422_programers2 {
 		
 		System.out.println(answer);
 
-		for (int i = 0; i < dot.length; i++) {
-			if(dot[i] >= 0) {
-				answer = 1;
-			} else if(dot[i] < 0) {
-				if(dot[0] < 0 && dot[1] >=0) {
-					answer = 2;
-				}
-			}
-		}
 
 	}
 }
