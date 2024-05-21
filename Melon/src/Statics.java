94eb2193c4edc4d66313727ce31fb48f0a465a31
yuna <yuna2101@naver.com>
2024-05-21 18:11:22 +0900

    스태틱수정

--- Melon/src/Statics.java
+++ Melon/src/Statics.java
@@ -1,7 +1,7 @@
 
 public class Statics {
-	public static String Melon_ID  = "yunas2101";
-	public static String Melon_PWD = "yu4198@m";
+	public static String Melon_ID  = "";
+	public static String Melon_PWD = "";
 
 
 }
