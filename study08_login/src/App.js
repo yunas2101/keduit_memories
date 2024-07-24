89828a412e26d8e80c6207c447f3e04fd9db21f7
yuna <yuna2101@naver.com>
2024-07-24 18:37:01 +0900

    0724 로그아웃, 회원탈퇴, 마이페이지, 이미지 첨부

--- study08_login/src/App.js
+++ study08_login/src/App.js
@@ -5,41 +5,50 @@ import { Index } from './components/Index/Index';
 import { Signup } from './components/Signup/Signup';
 import { useAuthStore } from './store/store';
 import { useEffect } from 'react';
+import { Loading } from './commons/Loading/Loading';
+import { useState } from 'react';
+import { Mypage } from './components/Mypage/Mypage';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+// axios가 요청을 보낼 때 쿠키값을 포함해서 전송하는 설정
 
 function App() {
 
   // useEffect <-- 중요도 높음
   // 마운팅 되는 시점, 인마운팅 되는 시점, 그리고 개발자가 정하는 특정 시점에만 실행되는 코드를 작성
-
-
-  const { setLoginID } = useAuthStore();
+  const { loginID, setLoginID } = useAuthStore();
+  const [isLoading, setIsLoading] = useState(true);
+  const [myInfo, setMyInfo] = useState({ id: '', pw: '', name: '' }); // 마이페이지 
 
   useEffect(() => {
     // 마운팅 되는 시점에 실행하고 싶은 코드
-
     const loginID = sessionStorage.getItem("loginID"); // sessionStorage 에서 아이디를 꺼내서 zustand store로 이전
     setLoginID(loginID);
-
-    return () => {
-      // 언마운팅 되는 시점에 실행하고 싶은 코드  (필요할때만 실행)
-    }
   }, []);
 
+  useEffect(() => {
+    setIsLoading(false);
+  }, [loginID])
 
 
+  if (isLoading) {
+    return <Loading />
+  } else {
+    return (
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route path='/' element={<Index setMyInfo={setMyInfo} />} />
+            <Route path='/member/signup' element={<Signup />} />
+            <Route path='/member/mypage' element={<Mypage myInfo={myInfo} />} />
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
 
 
-
-  return (
-    <div className="container">
-      <Router>
-        <Routes>
-          <Route path='/' element={<Index />} />
-          <Route path='/member/signup' element={<Signup />} />
-        </Routes>
-      </Router>
-    </div>
-  );
 }
 
 export default App;
--- study08_login/src/components/Index/Index.jsx
+++ study08_login/src/components/Index/Index.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import styles from "./Index.module.css";
-import { useNavigate } from 'react-router-dom';
+import { Router, useNavigate } from 'react-router-dom';
 import axios from "axios";
 import { useAuthStore } from "../../store/store";
+import { useEffect } from "react";
 
-export const Index = () => {
+export const Index = ({ setMyInfo }) => {
 
     const { loginID, setLoginID } = useAuthStore();
     const navi = useNavigate();
 
     const [auth, setAuth] = useState({ id: '', pw: '' });
+
 
     // 입력 (로그인)
     const handleChange = (e) => {
@@ -31,10 +33,59 @@ export const Index = () => {
         })
     }
 
+    // 로그아웃 
+    const handleLogout = () => {
+        axios.delete(`http://192.168.1.15/auth`).then(resp => {
+            sessionStorage.removeItem("loginID");
+            setLoginID('');
+        })
+    };
+
+    // 회원탈퇴
+    const handleSignout = () => {
+        if (!window.confirm("정말 탈퇴하시겠습니까?")) return;
+
+        axios.delete(`http://192.168.1.15/members`).then(resp => {
+            sessionStorage.removeItem("loginID");
+            setLoginID('');
+        })
+    }
+
+    // 마이페이지
+    const handleMypage = () => {
+        axios.get(`http://192.168.1.15/members`).then(resp => {
+            setMyInfo(resp.data);
+            navi("/member/mypage", { state: resp.data });
+        })
+    }
+
+
+
     return (
 
         <>
-            {loginID ? <div><h1>{loginID}님 환영합니다.</h1></div> :
+            {loginID ?
+                <table>
+                    <thead>
+                        <tr>
+                            <th colSpan={4}>
+                                <h1>{loginID} 님 환영합니다.</h1>
+                                <img src="http://192.168.1.15/images/test.png"></img>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><button>임시버튼</button></td>
+                            {/* <td><button onClick={() => { navi("/member/mypage") }}>마이페이지</button></td> */}
+                            <td><button onClick={handleMypage}>마이페이지</button></td>
+                            <td><button onClick={handleLogout}>로그아웃</button></td>
+                            <td><button onClick={handleSignout}>회원탈퇴</button></td>
+                        </tr>
+                    </tbody>
+
+                </table>
+                :
                 <table>
                     <thead>
                         <tr>
@@ -57,7 +108,6 @@ export const Index = () => {
                     </tbody>
                 </table>
             }
-
         </>
     )
 }
