e4715d164dfc6b3f45b831f6c140767e81c357b0
seunghye00 <tmdgp1934@gmail.com>
2024-08-29 00:01:39 +0900

    test

---
+++ src/main/java/com/kedu/KrakenPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.kedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrakenPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrakenPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kedu/KrakenPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrakenPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
