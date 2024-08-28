0bd3a09d834730ef8046b05e07755deeaa4b806c
seunghye00 <tmdgp1934@gmail.com>
2024-08-29 00:05:20 +0900

    연습용 파일 생성
    연습용 코드 작성

---
+++ src/main/java/com/kedu/controllers/PracticeController.java
@@ -0,0 +1,19 @@
+package com.kedu.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/practice")
+public class PracticeController {
+	
+	@PostMapping
+	public ResponseEntity<Void> practice(){	
+		System.out.println("안뇽");
+		return ResponseEntity.ok().build();
+	}
+}
