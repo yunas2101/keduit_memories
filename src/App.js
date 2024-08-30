297279e5b9c4582bc45ba2dcdfa25b2d38e90434
yuna <yuna2101@naver.com>
2024-08-30 17:37:40 +0900

    jwt 연습

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder='id'></input><br></br>
+      <input placeholder='pw'></input><br></br>
+      <button>로그인</button>
     </div>
   );
 }
